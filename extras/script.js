1453c69f2ce9f1597baa04c817d6f6cf625bf12c
Tobias-André <tobias.hole@gmail.com>
2024-01-25 12:56:46 +0100

    Mjau

--- extras/script.js
+++ extras/script.js
@@ -1,9 +1,79 @@
+// Github: https://github.com/Tobias-Andre
+
 const shipId = document.getElementById("shipId");
+const newShip = document.getElementById("newShip");
+const calcprice = document.getElementById("calcPrice"); 
+const remShip = document.getElementById("remShip");
 
-const fuel = Math.floor((Math.random() * 999999) + 100);
-const cargo = Math.floor((Math.random() * 99999999) + 10000);
+function firstEmptySpace() {
+    for (let i = 0; i < portSpaceEmpty.length; i++) {
+        if (portSpaceEmpty[i].state === "empty") {
+            portSpaceEmpty[i].state = "occupied";
+            let spot = portSpaceEmpty[i].space.padStart(2, "0")
+            portSpaceEmpty[i].id = `${spot} ${fuelStr} ${cargoStr}`
+            return spot;
+        }
+    }
+}
 
-let fuelStr = fuel.toString().padStart(6, '0');
-let cargoStr = cargo.toString().padStart(8, '0');
+function addNewShip() {
+    let fuelStr = fuel.toString().padStart(6, '0');
+    let cargoStr = cargo.toString().padStart(8, '0');
+    shipId.innerText = `${firstEmptySpace()} ${fuelStr} ${cargoStr}`;
+}
 
-shipId.innerText = `XX ${fuelStr} ${cargoStr}`;
+const fuel = Math.floor((Math.random() * 999900) + 100);
+const cargo = Math.floor((Math.random() * 99990000) + 10000);
+
+let portSpaceEmpty = [
+    {
+        space: "1",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "2",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "3",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "4",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "5",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "6",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "7",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "8",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "9",
+        state: "empty",
+        id: ""
+    },
+    {
+        space: "10",
+        state: "empty",
+        id: ""
+    }
+];
